3903f5cc311dd20f3a518354115e18ed8d19554c
Alicia Alexander <aliciaalexander@Alicias-MacBook-Pro.local>
2020-06-11 20:52:52 -0400

    Start of importing materialize css files and components and setup.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <h2>Bare start</h2>
+  <div>
+    <Navbar />
+    <h2>Bare start</h2>
+  </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
 export default {
-  name: 'App'
+  components: {
+    Navbar
+  }
 }
 </script>
 
